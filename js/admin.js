c07195c555f92533c1c4ef85b382de6df7a46bd6
shurik409 <shurik409@gmail.com>
2018-07-28 02:43:24 +0300

    some fix

--- js/admin.js
+++ js/admin.js
@@ -43,4 +43,28 @@ $(function(){
          });
         }
     });
+
+    $('.type-search').on('input propertychange',function(){
+        if($(this).val() != ''){
+            $('#list').slideDown();
+        }
+        else if($(this).val() == ''){
+            $('#list').slideUp();
+        }
+    });
+    $('.list_val').click(function(){
+        var LinkText = $(this).text();
+        var nowText = $('.type-search').val().split(',');
+        nowText.splice(nowText.length-1,1,' ');
+        var text = nowText.join(',');
+        $('.type-search').attr('value',text  + LinkText + ",");
+        $('#list').slideUp();
+
+        return false;
+    });
+    $(window).click(function(){
+
+        $('#list').slideUp();
+    });
 });
+
